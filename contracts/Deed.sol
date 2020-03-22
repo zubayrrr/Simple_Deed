3e06543ddc0c937dad8287ee6d2b8b489bfebf35
zubayrrr <abdulfaiz076@gmail.com>
2020-03-22 14:30:53 +0530

    inital commit

---
+++ contracts/Deed.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.5.0;
+
+contract Deed {
+    address public lawyer;
+    address payable public beneficiary;
+    uint256 public earliest;
+
+    constructor(address _lawyer, address payable _beneficiary, uint256 fromNow)
+        public
+        payable
+    {
+        lawyer = _lawyer;
+        beneficiary = _beneficiary;
+        earliest = now + fromNow;
+    }
+
+    function withdraw() public {
+        require(msg.sender == lawyer, "lawyer only");
+        require(now >= earliest, "too early");
+        beneficiary.transfer(address(this).balance);
+    }
+}
